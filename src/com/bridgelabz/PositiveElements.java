e5284e7dbfd2966a930ce57aedf8cbea2bb0d278
sukanya2712 <sukanyanaik511@gmail.com>
2023-03-06 22:32:30 +0530

    BASIC FILE ADDED

---
+++ src/com/bridgelabz/PositiveElements.java
@@ -0,0 +1,23 @@
+package com.bridgelabz;
+
+/*Write a prog to find sum of positive array given no is array like
+ [-1,-2,-4, 5 , 8, 9] find sum like [5,8,9] sum should be 23*/
+
+public class PositiveElements {
+    public static void main(String[] args) {
+
+        //Declaring the array with given values
+        int arr[]={-1,-2,-4,5,8,9};
+        int sumofPositiveEle=0;
+
+        //sum of positive elements in array as values greater than 0
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]>0) {
+                sumofPositiveEle = sumofPositiveEle + arr[i];
+            }
+        }
+
+
+        System.out.println("Sum of positive integers in array is" + sumofPositiveEle);
+    }
+}
